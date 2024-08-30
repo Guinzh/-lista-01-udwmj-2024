d9ce77b047eb6dd5191cd4d94bd18486f12e882a
Guinzh <130096690+Guinzh@users.noreply.github.com>
2024-08-30 17:57:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+//alert("ola daniel")
+//var resposta = confirm("Deseja excluir");
+//console.log(resposta);
+//var a= 10;
+//var b=33;
+//alert(a+b);
+
+function somar(a, b){
+
+	return a+ b;}
+console.log(somar(2, 2));
